bd63692b0154a76999c1ac8ab20857984b528e82
Adharsh Krishna <adharshrp@gmail.com>
2017-09-15 12:41:48 +0530

    Updated start_rc.py

--- start_rc.py
+++ start_rc.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
 
-# Resource object code
-#
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.7)
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore
 
